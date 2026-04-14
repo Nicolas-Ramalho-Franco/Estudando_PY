e70f7ae921729a635c10b3b17d3d7123cf641a34
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-13 22:20:05 -0300

    fiz o negocio

---
+++ tomada de decisão/For - laco de repeticao.py
@@ -0,0 +1,27 @@
+#for var in range(10,1,-1): #esta pegando de tras para frente
+    #print(var)
+#for var in range(0,51): #esta pegando de tras para frente
+ #   print(var)
+
+#for i in range (199,99,-2):
+ #   print(i)
+
+# min =0
+# max = 0
+# for i in range(10):
+#     num= int(input(f"Digite o {i+1}º numero"))
+#     if num > max:
+#         max = num
+#     elif num < min:
+#         min = num
+# print(f"seu numero maior e = {max} e o menor e ={min}")
+nota1 = -1
+nota2 =-1
+while nota1 <=-1 or nota2 <=-1 and nota1 >11 or nota2>11:
+    for i in range (3):
+        notaf1 = float(input("Gigite a sua nota1"))
+        notaf2 = float(input("Gigite a sua nota1"))
+ @
+        media = (notaf1 +notaf2)/30
+print(media)
+
